92b2326e6895a0916beed6399094897df5e5898c
Avigail Margolin <avigail.margolin@gmail.com>
2021-03-01 17:35:06 -0800

    fixing an issue in the sql file

--- server/server.test.js
+++ server/server.test.js
@@ -10,31 +10,21 @@ describe('testing-server-endpoints', () => {
     request.get('/api/items/5/reviews')
       .expect(200, [
         {
-          id: 15745511,
-          customer_name: 'Clifford Flatley',
-          date_of_review: 'May 5 2020',
+          id: 9,
+          customer_name: 'Shlomi Harazi',
+          date_of_review: 'Jan 3 2020',
           rating: 1,
-          review_content: 'Voluptates nulla facilis qui cumque reprehenderit ipsum repudiandae perspiciatis.',
-          image_url: 'https://fetsy-reviews-sdc.s3.us-east-2.amazonaws.com/images/479.jpg',
-          item_option: 'dicta itaque reiciendis',
-          itemid: 5,
-        },
-        {
-          id: 41474819,
-          customer_name: 'Constance Parker',
-          date_of_review: 'Nov 1 2020',
-          rating: 2,
-          review_content: 'Libero ratione accusamus.',
-          image_url: 'https://fetsy-reviews-sdc.s3.us-east-2.amazonaws.com/images/847.jpg',
-          item_option: 'aut magnam aut',
-          itemid: 5,
-        },
+          review_content: 'low quality',
+          image_url: 'https://fetsy-reviews-sdc.s3.us-east-2.amazonaws.com/images/899.jpg',
+          item_option: 'Blue',
+          itemid: 5
+        }
       ], done);
   }); // it
 
   it('POST a new review to a specific item by the itemId', (done) => {
     request
-      .post('/api/items/60/reviews')
+      .post('/api/items/9/reviews')
       .send({
         customer_name: 'Shalom Meshulam',
         date_of_review: 'May 3 2020',
@@ -55,7 +45,7 @@ describe('testing-server-endpoints', () => {
   it('UPDATE/PUT an item review by the itemId and review id', (done) => {
     request
     // UPDATE - '/api/items/:itemId/reviews/:reviewId'
-      .put('/api/items/15/reviews/12665511')
+      .put('/api/items/8/reviews/8')
       .send({
         customer_name: 'Rick Morar',
         date_of_review: 'July 10 2020',
@@ -65,7 +55,7 @@ describe('testing-server-endpoints', () => {
         item_option: 'culpa et nesciunt',
       })
       .expect(200, {
-        message: 'Review 12665511 was updated successfully.',
+        message: 'Review 8 was updated successfully.',
       })
       .end((err, res) => {
         if (err) return done(err);
@@ -75,12 +65,12 @@ describe('testing-server-endpoints', () => {
 
   it('Delete a review from a specific item by using itemId and review id', (done) => {
     request
-      .get('/api/items/60/reviews')
+      .get('/api/items/2/reviews')
       .then(response => {
         //console.log(response.body[0].id);
         let reviewId = response.body[0].id;
         return request
-          .delete(`/api/items/60/reviews/${reviewId}`)
+          .delete(`/api/items/2/reviews/${reviewId}`)
           .expect(200, {
             message: `Review with id ${reviewId} was deleted successfully!`,
           })
